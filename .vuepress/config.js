903cbb06e82f20b02380c63031ebae78d9f82b1d
Rob McCormack <mrmccormack@gmail.com>
2018-10-03 14:11:35 -0400

    initial setup

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "Welcome to Wakanda",
+  description: "The cultured people of Wakanda welcome you"
+}
+
+
+// .vuepress/config.js
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
